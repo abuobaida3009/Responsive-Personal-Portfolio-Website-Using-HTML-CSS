511ca3ca3c0906f51be28edc7235235bfe8f76c4
abuobaida3009 <abu..obaida3009@gmail.com>
2023-10-09 13:22:46 +0600

    about section commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//get tablinks & tab contents
+var tabLinks = document.getElementsByClassName("tab-links"),
+tabContents = document.getElementsByClassName("tab-contents");
+//on open tab function call
+function openTab(tabname){
+    for(item of tabLinks){
+        item.classList.remove("active-link");
+    }
+    for(item of tabContents){
+        item.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
